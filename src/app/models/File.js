00b37dbb151d1e1a92d24a551c9b36b4bcd06895
denis macedo <denisrmacedo@gmail.com>
2020-02-25 02:17:22 -0300

    appointments functions

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -7,6 +7,12 @@ class File extends Model {
         // colunas da tabela
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get(){
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       { sequelize },
     );
